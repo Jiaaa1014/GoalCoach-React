a6df6fcd6849f16dbeb79ba18bb88f88fb0f406f
Jiaaa1014 <linao264590000@gmail.com>
2018-01-17 19:07:14 +0800

    modi

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import { completeGoalRef, goalRef } from '../firebase'
 
 class GoalItem extends Component {
@@ -32,5 +33,13 @@ class GoalItem extends Component {
 function mapStateToProps(state) {
   const { user } = state
   return { user }
+}
+GoalItem.PropTypes = {
+  goal: PropTypes.object,
+  user: PropTypes.object,
+  email: PropTypes.string,
+  title: PropTypes.string,
+  serverKey: PropTypes.string
+
 }
 export default connect(mapStateToProps, null)(GoalItem)
