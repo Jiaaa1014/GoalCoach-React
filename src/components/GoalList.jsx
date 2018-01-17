5c59bc541ef53448c0982b4e43c8c79e766cd5c9
Jiaaa1014 <linao264590000@gmail.com>
2018-01-17 21:32:50 +0800

    eventually 2

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import { goalRef } from '../firebase'
 import { setGoals } from '../actions'
 import GoalItem from './GoalItem'
 
 class GoalList extends Component {
+  static propTypes = {
+    setGoals: PropTypes.func.isRequired,
+    goals: PropTypes.object.isRequired // eslint-disable-line react/forbid-prop-types
+  }
   componentDidMount() {
-    goalRef.on('value', snap => {
-      let goals = []
+    goalRef.on('value', (snap) => {
+      const goals = []
       snap.forEach((goal) => {
         const { email, title } = goal.val() // 沒有val()，看起來會是Object{node:{...}, ref:{...}}
         const serverKey = goal.ref.path.pieces_[1]
@@ -19,9 +24,9 @@ class GoalList extends Component {
   render() {
     return (
       <div>
-        {this.props.goals.map((goal, key) => {
+        {this.props.goals.map((goal, i) => {
           return (
-            <GoalItem key={key} goal={goal} />
+            <GoalItem key={i} goal={goal} />
           )
         })}
       </div>
